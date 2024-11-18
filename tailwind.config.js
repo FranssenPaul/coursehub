1d76e12a22edaf635b41bfbb2951ce0175186c80
FranssenPaul <franssen.maths@gmail.com>
2024-11-18 10:04:59 +0100

    refactor bases

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui";
+export default {
+  content: [
+    "./index.html", // Assure que Tailwind scanne index.html dossier public
+    "./src/**/*.{js,ts,jsx,tsx}", // Scanne tous les fichiers de composants
+    // dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "coffee",
+      "light",
+      "forest",
+      "bumblebee",
+      "emerald",
+      "halloween",
+      "wireframe",
+      "autumn",
+      "coffee",
+      "business",
+      "cupcake",
+      "corporate",
+    ],
+  },
+};
